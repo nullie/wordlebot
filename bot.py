0bd325966b28ca2086962483f8006cb612e02850
Ilia Novoselov <ilya.novoselov@gmail.com>
2022-07-06 21:18:39 +0500

    Initial release

---
+++ bot.py
@@ -0,0 +1,95 @@
+from typing import List
+import irc.bot
+import irc.strings
+import irccodes
+
+import wordle
+
+
+BACKGROUND_COLORS = {
+    wordle.Status.MISS: ('white', 'grey'),
+    wordle.Status.RIGHT_PLACE: ('white', 'green'),
+    wordle.Status.RIGHT_LETTER: ('black', 'yellow'),
+}
+
+
+def format_wordle_hint(guess: List[wordle.Letter]):
+    return ' '.join(
+        irccodes.colored(
+            ' ' + irccodes.bold(letter.letter) + ' ',
+            *BACKGROUND_COLORS[letter.status], '')
+        for letter in guess
+    )
+
+
+class TestBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, channel, nickname, server, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(
+            self, [(server, port)], nickname, nickname)
+        self.channel = channel
+        self.wordle = wordle.Game()
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+        c.privmsg(self.channel, format_wordle_hint(self.wordle.hint()))
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        a = e.arguments[0].split(":", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(
+            self.connection.get_nickname()
+        ):
+            self.do_command(e, a[1].strip())
+        return
+
+    def do_command(self, e, cmd):
+        c = self.connection
+
+        try:
+            correct, hint = self.wordle.guess(cmd.lower())
+        except wordle.GuessError as e:
+            c.privmsg(self.channel, e.args[0])
+        else:
+            if correct:
+                nick = e.source.nick
+                self.wordle.restart()
+                hint = self.wordle.hint()
+                c.privmsg(
+                    self.channel,
+                    f'Congrats, {nick} won, next word: {format_wordle_hint(hint)}'
+                )
+            else:
+                c.privmsg(self.channel, format_wordle_hint(hint))
+
+
+def main():
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: testbot <server[:port]> <channel> <nickname>")
+        sys.exit(1)
+
+    s = sys.argv[1].split(":", 1)
+    server = s[0]
+    if len(s) == 2:
+        try:
+            port = int(s[1])
+        except ValueError:
+            print("Error: Erroneous port.")
+            sys.exit(1)
+    else:
+        port = 6667
+    channel = sys.argv[2]
+    nickname = sys.argv[3]
+
+    bot = TestBot(channel, nickname, server, port)
+    bot.start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ wordle.py
@@ -0,0 +1,72 @@
+from collections import Counter
+from enum import Enum, auto
+import random
+import re
+from typing import Iterator, List, NamedTuple, Tuple
+
+
+class Status(Enum):
+    MISS = auto()
+    RIGHT_PLACE = auto()
+    RIGHT_LETTER = auto()
+
+
+class Letter(NamedTuple):
+    letter: str
+    status: Status
+
+
+class Game:
+    def __init__(self) -> None:
+        self.words = set(load_good_words())
+        self.word = random.choice(list(self.words))
+
+    def restart(self):
+        self.word = random.choice(list(self.words))
+
+    def hint(self) -> List[Letter]:
+        return [Letter(' ', Status.MISS) for _ in self.word]
+
+    def guess(self, guess: str) -> Tuple[bool, List[Letter]]:
+        if len(guess) != len(self.word):
+            raise GuessError('wrong length')
+
+        if guess not in self.words:
+            raise GuessError('not in the word list')
+
+        word_counts = Counter(self.word)
+        right_place = set()
+        for i, (word_letter, guess_letter) in enumerate(zip(self.word, guess)):
+            if word_letter == guess_letter:
+                right_place.add(i)
+                word_counts[guess_letter] -= 1
+
+        right_letter = set()
+        for i, letter in enumerate(guess):
+            if i not in right_place and word_counts[letter] > 0:
+                right_letter.add(i)
+                word_counts[letter] -= 1
+
+        return guess == self.word, [
+            Letter(
+                letter,
+                (
+                    Status.RIGHT_PLACE if i in right_place
+                    else Status.RIGHT_LETTER if i in right_letter
+                    else Status.MISS
+                )
+            )
+            for i, letter in enumerate(guess)
+        ]
+
+
+class GuessError(Exception):
+    pass
+
+
+def load_good_words() -> Iterator[str]:
+    with open('/usr/share/dict/american-english') as f:
+        for word in f:
+            word = word.strip().lower()
+            if re.fullmatch('[a-z]+', word) and 4 <= len(word) <= 8:
+                yield word
