b2a17ac0f32826a4789f5fdce6a8d29ca04c955e
Ilia Novoselov <i.novoselov@eclecticiq.com>
2023-10-24 22:50:22 +0200

    Fixed flake

--- flake.nix
+++ flake.nix
@@ -8,35 +8,59 @@
     self,
     nixpkgs,
     flake-utils,
+    poetry2nix,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+      };
     in {
       packages.wordlebot = pkgs.poetry2nix.mkPoetryApplication {
         projectDir = ./.;
         overrides = pkgs.poetry2nix.overrides.withDefaults (
           self: super: {
-            irccodes = super.irccodes.overridePythonAttrs (
-              {
+            irccodes = super.irccodes.overridePythonAttrs {
+              format = "setuptools";
+            };
+            autocommand = super.autocommand.overridePythonAttrs {
+              format = "setuptools";
+            };
+            annotated-types = super.annotated-types.overrideAttrs (
+              old: {
                 format = "setuptools";
+                nativeBuildInputs =
+                  (old.nativeBuildInputs or [])
+                  ++ [
+                    self.hatchling
+                    self.flake8
+                  ];
               }
             );
-            autocommand = super.autocommand.overridePythonAttrs (
-              {
-                format = "setuptools";
+            pydantic = super.pydantic.overridePythonAttrs (
+              old: {
+                format = "pyproject";
+                nativeBuildInputs =
+                  (old.nativeBuildInputs or [])
+                  ++ [
+                    self.hatchling
+                    self.hatch-fancy-pypi-readme
+                  ];
               }
             );
-            annotated-types = super.annotated-types.overridePythonAttrs (
-              {
-                format = "other";
-                # nativeBuildInputs = [super.hatchling super.flake8];
-                dontBuild = true;
-                buildPhase = null;
-              }
-            );
-            pydantic-core = super.annotated-types.overridePythonAttrs (
-              {
-                nativeBuildInputs = [pkgs.maturin];
+            pydantic-core = super.pydantic-core.overridePythonAttrs (
+              old: {
+                format = "pyproject";
+                cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
+                  src = old.src;
+                  hash = "sha256-X2s/VwM5emCq2bcEqcezoC9wnoP6UU0CgeM0x2PlzgI=";
+                };
+                nativeBuildInputs =
+                  (old.nativeBuildInputs or [])
+                  ++ [
+                    pkgs.maturin
+                    pkgs.rustPlatform.cargoSetupHook
+                    pkgs.rustPlatform.maturinBuildHook
+                  ];
               }
             );
           }
