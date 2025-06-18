5bacc2f62467c8d4124706a7f4e600a512ea79f8
Ilia Novoselov <ilya.novoselov@gmail.com>
2025-06-19 01:49:33 +0200

    Switched to uv and updated dependencies

--- flake.nix
+++ flake.nix
@@ -2,75 +2,100 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    poetry2nix,
+    uv2nix,
+    pyproject-nix,
+    pyproject-build-systems,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
-    in {
-      packages.wordlebot = pkgs.poetry2nix.mkPoetryApplication {
-        projectDir = ./.;
-        overrides = pkgs.poetry2nix.overrides.withDefaults (
-          self: super: {
-            irccodes = super.irccodes.overridePythonAttrs {
-              format = "setuptools";
-            };
-            autocommand = super.autocommand.overridePythonAttrs {
-              format = "setuptools";
-            };
-            annotated-types = super.annotated-types.overrideAttrs (
-              old: {
-                format = "setuptools";
-                nativeBuildInputs =
-                  (old.nativeBuildInputs or [])
-                  ++ [
-                    self.hatchling
-                    self.flake8
-                  ];
-              }
-            );
-            pydantic = super.pydantic.overridePythonAttrs (
-              old: {
-                format = "pyproject";
-                nativeBuildInputs =
-                  (old.nativeBuildInputs or [])
-                  ++ [
-                    self.hatchling
-                    self.hatch-fancy-pypi-readme
-                  ];
-              }
-            );
-            pydantic-core = super.pydantic-core.overridePythonAttrs (
-              old: {
-                format = "pyproject";
-                cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
-                  src = old.src;
-                  hash = "sha256-X2s/VwM5emCq2bcEqcezoC9wnoP6UU0CgeM0x2PlzgI=";
-                };
-                nativeBuildInputs =
-                  (old.nativeBuildInputs or [])
-                  ++ [
-                    pkgs.maturin
-                    pkgs.rustPlatform.cargoSetupHook
-                    pkgs.rustPlatform.maturinBuildHook
-                  ];
-              }
-            );
-          }
-        );
+
+      inherit (nixpkgs) lib;
+
+      workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
+
+      # Create package overlay from workspace.
+      overlay = workspace.mkPyprojectOverlay {
+        # Prefer prebuilt binary wheels as a package source.
+        # Sdists are less likely to "just work" because of the metadata missing from uv.lock.
+        # Binary wheels are more likely to, but may still require overrides for library dependencies.
+        sourcePreference = "wheel"; # or sourcePreference = "sdist";
+        # Optionally customise PEP 508 environment
+        # environ = {
+        #   platform_release = "5.10.65";
+        # };
       };
 
+      pyprojectOverrides = _final: _prev: {
+        # Implement build fixups here.
+        # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
+        # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
+      };
+
+      python = pkgs.python313;
+
+      pythonSet =
+        # Use base package set from pyproject.nix builders
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        })
+        .overrideScope
+        (
+          lib.composeManyExtensions [
+            pyproject-build-systems.overlays.default
+            overlay
+            pyprojectOverrides
+          ]
+        );
+    in {
+      packages.wordlebot = pythonSet.mkVirtualEnv "wordlebot" workspace.deps.default;
       defaultPackage = self.packages.${system}.wordlebot;
 
       devShell = pkgs.mkShell {
-        inputsFrom = builtins.attrValues self.packages.${system};
+        packages = [
+          python
+          pkgs.uv
+        ];
+        env =
+          {
+            # Prevent uv from managing Python downloads
+            UV_PYTHON_DOWNLOADS = "never";
+            # Force uv to use nixpkgs Python interpreter
+            UV_PYTHON = python.interpreter;
+          }
+          // lib.optionalAttrs pkgs.stdenv.isLinux {
+            # Python libraries often load native shared objects using dlopen(3).
+            # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+          };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
       };
 
       formatter = pkgs.alejandra;
